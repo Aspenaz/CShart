27c52704cee651c6ac20978cb819417f3cd57789
Aspenaz <miguelvelasquez@protonmail.com>
2023-07-22 11:19:48 -0500

    ...

--- tutorialesprogramacionya.com/22.Matrices(CantidadDeFilasYColumnas)/Problema1/Problema1/Matriz5.cs
+++ tutorialesprogramacionya.com/22.Matrices(CantidadDeFilasYColumnas)/Problema1/Problema1/Matriz5.cs
@@ -7,9 +7,61 @@ using System;
 
 class Matriz5
 {
+    private int[,] matriz;
+
+    public void Cargar()
+    {
+        Console.Write("Ingrese cantidad de filas: ");
+        int filas = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa cantida de columnas: ");
+        int columnas = int.Parse(Console.ReadLine());
+
+        matriz = new int[filas, columnas];
+
+        for(int c = 0; c < matriz.GetLength(0); c++)
+        {
+            for (int f = 0; f < matriz.GetLength(1); f++)
+            {
+                Console.Write($"Ingrese componente: ");
+                matriz[f,c] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void Imprimir()
+    {
+        for (int f = 0; f < matriz.GetLength(0); f++)
+        {
+            for (int c = 0; c < matriz.GetLength(1); c++)
+            {
+                Console.Write($"{matriz[f, c], 2} ");
+            }
+            Console.WriteLine();    
+        }
+    }
+
+
+    public void ImprimeUltimaFila()
+    {
+        Console.WriteLine("\nUltima fila");
+
+        for(int c = 0; c < matriz.GetLength(1) - 1; c++)
+        {
+            Console.Write(matriz[matriz.GetLength(0) - 1, c] + " ");
+        }
+    }
+
     static void Main()
     {
+        Matriz5 m = new Matriz5();
 
+        m.Cargar();
+        m.Imprimir();
+        m.ImprimeUltimaFila();
+
+        Console.ReadKey();
     }
 }
 
