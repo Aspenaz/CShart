3f42dbc76a0268a7a08259cb13b1354bb0c9aaa6
Miguel Velásquez <125136335+Aspenaz@users.noreply.github.com>
2023-07-22 22:18:11 -0500

    ...
    ...

---
+++ tutorialesprogramacionya.com/22.Matrices(CantidadDeFilasYColumnas)/1/1/Matriz7.cs
@@ -0,0 +1,72 @@
+﻿/* 1. Crear una matriz de n * m filas (cargar n y m por teclado) Intercambiar 
+ *    la primer fila con la segunda. Imprimir luego la matriz.
+ */
+
+
+using System;
+
+class Matriz7
+{
+    private int[,] matriz;
+
+    public void Cargar()
+    {
+        Console.Write("Ingrese cantindad de filas: ");
+        int filas = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese cantidad de columnas: ");
+        int columnas = int.Parse(Console.ReadLine());
+
+        matriz = new int[filas, columnas];
+
+        for(int f = 0; f < matriz.GetLength(0); f++)
+        {            
+            for(int c = 0; c < matriz.GetLength(1); c++)
+            {
+                Console.Write("Ingrese componente: ");
+                matriz[f,c] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+    public void IntercambiarFila1y2()
+    {
+        for(int c = 0; c < matriz.GetLength(1); c++)
+        {
+            int aux = matriz[0, c];
+            matriz[0, c] = matriz[1, c];
+            matriz[1, c] = aux;
+        }
+    }
+
+    public void Imprimir()
+    {
+        for(int f = 0; f < matriz.GetLength(0); f++)
+        {
+            for (int c = 0; c < matriz.GetLength(1); c++)
+            {
+                Console.Write(matriz[f, c] + " ");
+            }
+            Console.WriteLine();
+        }            
+    }
+
+
+
+
+
+    static void Main()
+    {
+        Matriz7 m = new Matriz7();  
+
+        m.Cargar(); 
+        m.Imprimir();   
+        m.IntercambiarFila1y2();
+        m.Imprimir();   
+
+        Console.ReadKey();
+    }
+}
+
---
+++ tutorialesprogramacionya.com/22.Matrices(CantidadDeFilasYColumnas)/Problema2/Problema2/Matriz6.cs
@@ -0,0 +1,67 @@
+﻿/* Problema 2:
+    Crear una matriz de n * m filas (cargar n y m por teclado). 
+    Imprimir el mayor elemento y la fila y columna donde se almacena.
+*/
+
+
+using System;
+
+class Matriz6
+{
+    private int[,] matriz;
+
+    public void Cargar()
+    {
+        Console.Write("Ingrese cantidad de filas: ");
+        int filas = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese cantidad de columnas: ");
+        int columnas = int.Parse(Console.ReadLine());   
+
+        matriz = new int[filas,columnas];
+
+        for (int f = 0; f < matriz.GetLength(0); f++)
+        {
+            for (int c = 0; c < matriz.GetLength(1); c++)
+            {
+                Console.Write("Ingrese componente: ");
+                matriz[f,c] = int.Parse(Console.ReadLine());    
+            }
+        }
+    }
+
+    public void ImprimirMayor()
+    {
+        int mayor = matriz[0,0];
+        int filaMayor = 0;
+        int columnaMayor = 0;
+
+        for(int f = 0; f < matriz.GetLength(0); f++)
+        {
+            for(int c = 0;c < matriz.GetLength(1); c++)
+            {
+                if (matriz[f,c] > mayor)
+                {
+                    mayor = matriz[f,c];
+                    filaMayor = f;
+                    columnaMayor = c;   
+                }
+            }
+        }
+        Console.WriteLine("El elemento mayor es: " + mayor);
+        Console.WriteLine("Se encuentra en la fima: " + filaMayor + " y en la columna: " + columnaMayor);
+        Console.ReadLine();
+    }
+
+
+    static void Main()
+    {
+        Matriz6 m = new Matriz6();
+
+        m.Cargar();
+        m.ImprimirMayor();
+
+        Console.ReadLine();
+    }
+}
+
